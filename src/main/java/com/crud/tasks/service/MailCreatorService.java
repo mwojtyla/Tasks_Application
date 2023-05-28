40ed275d807912e017ba1394cb8c368da4616335
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2023-05-28 11:04:52 +0200

    32.3 Added file: created-tasks-information-mail.html. Modified classes: MailCreatorService, SimpleMailService

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -2,11 +2,19 @@ package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.config.CompanyDetailsConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.scheduler.EmailScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MailCreatorService {
@@ -16,11 +24,17 @@ public class MailCreatorService {
 
     @Autowired
     private CompanyDetailsConfig companyDetailsConfig;
+
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
@@ -28,6 +42,21 @@ public class MailCreatorService {
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("company_details", companyDetailsConfig.getCompanyName() + ", e-mail: " + companyDetailsConfig.getCompanyEmail() + ", phone: "+ companyDetailsConfig.getCompanyPhone());
         context.setVariable("preview_message", "New card in Trello created");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality",functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildTrelloInformationEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
+        context.setVariable("company_details", companyDetailsConfig.getCompanyName() + ", e-mail: " + companyDetailsConfig.getCompanyEmail() + ", phone: "+ companyDetailsConfig.getCompanyPhone());
+        context.setVariable("preview_message", "Information about the numbers of tasks");
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        return templateEngine.process("mail/created-tasks-information-mail", context);
     }
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,14 +1,16 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,7 +19,13 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class SimpleEmailService {
+
     private final JavaMailSender javaMailSender;
+
+    private final TaskRepository taskRepository;
+    private final AdminConfig adminConfig;
+
+    private static final String SUBJECT = "Daily mail: amount of tasks in database";
 
     @Autowired
     private MailCreatorService mailCreatorService;
@@ -33,7 +41,32 @@ public class SimpleEmailService {
         }
     }
 
-   /* private SimpleMailMessage createMailMessage(final Mail mail) {
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInfoMail() {
+        long size = taskRepository.count();
+
+        String taskWord = " task";
+        if (size>1){
+            taskWord = " tasks";
+        }
+
+        log.info("Starting email preparation...");
+        try {
+            javaMailSender.send(createMimeMessageInfo(new Mail(
+                    adminConfig.getAdminMail(),
+                    SUBJECT,
+                    "Currently in database you have: " + size + taskWord,
+                    null
+                    )
+            ));
+            log.info("Email has been sent.");
+        } catch (MailException e) {
+            log.error("Failed to process email sending: " + e.getMessage(), e);
+        }
+    }
+
+
+    /*private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
@@ -54,6 +87,15 @@ public class SimpleEmailService {
             messageHelper.setTo(mail.getMailTo());
             messageHelper.setSubject(mail.getSubject());
             messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+    private MimeMessagePreparator createMimeMessageInfo (final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloInformationEmail(mail.getMessage()), true);
         };
     }
 }
